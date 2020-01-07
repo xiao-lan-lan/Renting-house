a2240017c88562468978c65cc8b6d7e8c9b69abc
兰小兰 <479323417@qq.com>
2020-01-07 19:50:22 +0800

    tabbar配置完成

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,28 +1,112 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 // 引入路由组件
-import { Link, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 // 引入二级路由组件
-import Index from '../index';
-import Houselist from '../houselist';
-import Message from '../message';
-import Profile from '../profile';
+import Index from "../index";
+import Houselist from "../houselist";
+import Message from "../message";
+import Profile from "../profile";
+
+// 引入tabbar
+import { TabBar } from "antd-mobile";
+
+// 引入字体图标
+import "../../assets/fonts/iconfont.css";
 
 class Home extends React.Component {
+  // 状态数据
+  state = {
+    selectedTab: this.props.location.pathname,
+    hidden: false
+  };
+
   render() {
     return (
       <div className="home">
-        <div>
+        {/* link导航不需要,用 tabbar 代替 */}
+        {/* <div>
           <Link to="/home/index">home</Link>
           <Link to="/home/houselist">houselist</Link>
           <Link to="/home/message">message</Link>
           <Link to="/home/profile">profile</Link>
-        </div>
+        </div> */}
+
         <div className="route">
           <Route path="/home/index" component={Index} />
           <Route path="/home/houselist" component={Houselist} />
           <Route path="/home/message" component={Message} />
           <Route path="/home/profile" component={Profile} />
+        </div>
+
+        {/* tabbar */}
+        <div
+          style={{ position: "fixed", height: "100%", width: "100%", top: 0 }}
+        >
+          <TabBar
+            unselectedTintColor="#949494"
+            tintColor="#0eb36f"
+            barTintColor="white"
+            hidden={this.state.hidden}
+          >
+            <TabBar.Item
+              title="首页"
+              key="Life"
+              icon={<i className="iconfont icon-ind"></i>}
+              selectedIcon={<i className="iconfont icon-ind"></i>}
+              selected={this.state.selectedTab === "/home/index"}
+              onPress={() => {
+                // 路由跳转
+                this.props.history.push('/home/index')
+                // 选中tabbae
+                this.setState({
+                  selectedTab: "/home/index"
+                });
+              }}
+              data-seed="logId"
+            ></TabBar.Item>
+            <TabBar.Item
+              icon={<i className="iconfont icon-findHouse" />}
+              selectedIcon={<i className="iconfont icon-findHouse" />}
+              title="找房"
+              key="Koubei"
+              selected={this.state.selectedTab === "/home/houselist"}
+              onPress={() => {
+                this.props.history.push('/home/houselist')
+                this.setState({
+                  selectedTab: "/home/houselist"
+                });
+              }}
+              data-seed="logId1"
+            ></TabBar.Item>
+            <TabBar.Item
+              icon={<i className="iconfont icon-mess" />}
+              selectedIcon={<i className="iconfont icon-mess" />}
+              title="资讯"
+              key="Friend"
+              selected={this.state.selectedTab === "/home/message"}
+              onPress={() => {
+                this.props.history.push('/home/message')
+                this.setState({
+                  selectedTab: "/home/message"
+                });
+              }}
+            ></TabBar.Item>
+            <TabBar.Item
+              icon={<i className="iconfont icon-my" />}
+              selectedIcon={<i className="iconfont icon-my" />}
+              title="我的"
+              key="my"
+              selected={this.state.selectedTab === "/home/profile"}
+              onPress={() => {
+                this.props.history.push('/home/profile')
+                this.setState({
+                  selectedTab: "/home/profile"
+                });
+              }}
+            ></TabBar.Item>
+          </TabBar>
         </div>
       </div>
     );
