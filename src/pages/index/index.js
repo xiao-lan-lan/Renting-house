901d62a0b903621d3258120ad4800ba66f5e4f2c
兰小兰 <479323417@qq.com>
2020-01-07 22:26:14 +0800

    首页-轮播图请求完成

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,58 +1,64 @@
 import React from "react";
+import axios from "axios";
 
 // 轮播图
 import { Carousel } from "antd-mobile";
 
 class Index extends React.Component {
   state = {
-    data: ["1", "2", "3"],
+    swiperList: [],
     imgHeight: 176
   };
 
+  // 请求轮播图
+  loadSwiper = async () => {
+    const {data:res} = await axios({
+      method: "get",
+      url: "http://localhost:8080/home/swiper"
+    });
+    // 失败
+    if (res.status!==200) {
+      console.log('获取轮播图失败');
+    }
+    // 成功，修改数据
+    this.setState({
+      swiperList:res.body
+    })
+    
+  };
+
   componentDidMount() {
-    // simulate img loading
-    setTimeout(() => {
-      this.setState({
-        data: [
-          "AiyWuByWklrrUDlFignR",
-          "TekJlZRVCjLFexlOCuWn",
-          "IJOtIlfsYdTyaDTRVrLI"
-        ]
-      });
-    }, 100);
+    this.loadSwiper()
   }
 
   render() {
     return (
       <div className="home-index">
         {/* 轮播图 */}
-          <Carousel
-            autoplay={true}
-            infinite
-          >
-            {this.state.data.map(val => (
-              <a
-                key={val}
-                href="http://www.alipay.com"
-                style={{
-                  display: "inline-block",
-                  width: "100%",
-                  height: this.state.imgHeight
+        <Carousel autoplay={true} infinite>
+          {this.state.swiperList.map(swiper => (
+            <a
+              key={swiper.id}
+              href="http://www.baidu.com"
+              style={{
+                display: "inline-block",
+                width: "100%",
+                height: this.state.imgHeight
+              }}
+            >
+              <img
+                src={`http://localhost:8080${swiper.imgSrc}`}
+                alt=""
+                style={{ width: "100%", verticalAlign: "top" }}
+                onLoad={() => {
+                  // fire window resize event to change height
+                  window.dispatchEvent(new Event("resize"));
+                  this.setState({ imgHeight: "auto" });
                 }}
-              >
-                <img
-                  src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-                  alt=""
-                  style={{ width: "100%", verticalAlign: "top" }}
-                  onLoad={() => {
-                    // fire window resize event to change height
-                    window.dispatchEvent(new Event("resize"));
-                    this.setState({ imgHeight: "auto" });
-                  }}
-                />
-              </a>
-            ))}
-          </Carousel>
+              />
+            </a>
+          ))}
+        </Carousel>
 
         {/* 顶部搜索 */}
         {/* 四个导航 */}
