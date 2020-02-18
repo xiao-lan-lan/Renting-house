0ce9016a87416ab7e27e071d2e8cdbc75b5f7534
兰小兰 <479323417@qq.com>
2020-02-18 13:46:48 +0800

    找房-显示筛选栏

--- src/pages/houselist/components/FilterTitle/index.js
+++ src/pages/houselist/components/FilterTitle/index.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React from "react";
 
-import { Flex } from 'antd-mobile'
+import { Flex } from "antd-mobile";
 
-import styles from './index.module.css'
+import styles from "./index.module.css";
 
 // 条件筛选栏标题数组：
 const titleList = [
-  { title: '区域', type: 'area' },
-  { title: '方式', type: 'mode' },
-  { title: '租金', type: 'price' },
-  { title: '筛选', type: 'more' }
-]
+  { title: "区域", type: "area" },
+  { title: "方式", type: "mode" },
+  { title: "租金", type: "price" },
+  { title: "筛选", type: "more" }
+];
 
 export default function FilterTitle() {
   return (
     <Flex align="center" className={styles.root}>
-      <Flex.Item>
-        {/* 选中类名： selected */}
-        <span className={[styles.dropdown, styles.selected].join(' ')}>
-          <span>区域</span>
-          <i className="iconfont icon-arrow" />
-        </span>
-      </Flex.Item>
+      {titleList.map(title => {
+        return (
+          <Flex.Item key={title.type}>
+            {/* 选中类名： selected */}
+            <span className={[styles.dropdown].join(" ")}>
+              <span>{title.title}</span>
+              <i className="iconfont icon-arrow" />
+            </span>
+          </Flex.Item>
+        );
+      })}
     </Flex>
-  )
+  );
 }
