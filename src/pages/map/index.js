dddcca160eca2996070edbc8292cc1d477af21c9
兰小兰 <479323417@qq.com>
2020-02-20 14:57:16 +0800

    地图找房-创建覆盖物

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -1,13 +1,41 @@
 import React from "react";
 import { NavBar, Icon } from "antd-mobile";
 import "./index.scss";
+import { getCurrentCity } from "../../utils/getCurrentCity";
 
 class Map extends React.Component {
+  // 初始化地图
   initMap() {
     const { BMap } = window;
     var map = new BMap.Map("container");
     var point = new BMap.Point(116.404, 39.915);
     map.centerAndZoom(point, 15);
+
+    // 添加地图控件
+    map.addControl(new BMap.NavigationControl());
+    map.addControl(new BMap.ScaleControl());
+    map.addControl(new BMap.OverviewMapControl());
+    map.addControl(new BMap.MapTypeControl());
+
+    // 创建地址解析器实例
+    const myGeo = new BMap.Geocoder();
+    // 获取当前城市
+    let city
+    getCurrentCity((currcity)=>{
+      city = currcity
+    })
+    // 将地址解析结果显示在地图上，并调整地图视野
+    myGeo.getPoint(
+      city,
+      function(point) {
+        if (point) {
+          map.centerAndZoom(point, 16);
+          // 覆盖物
+          map.addOverlay(new BMap.Marker(point));
+        }
+      },
+      city
+    );
   }
   componentDidMount() {
     this.initMap();
