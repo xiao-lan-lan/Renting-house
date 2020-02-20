70f339b9144819c16891842340ff811c44f1973a
兰小兰 <479323417@qq.com>
2020-02-20 14:12:36 +0800

    发布房源-搜索页

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 // 一级路由组件
 import Home from "./pages/home";
 import citylist from "./pages/cityList";
-import Map from './pages/map';
-import HouseDetail from './pages/HouseDetail';
-import Login from './pages/Login';
-import Rent from './pages/Rent';
+import Map from "./pages/map";
+import HouseDetail from "./pages/HouseDetail";
+import Login from "./pages/Login";
+import Rent from "./pages/Rent";
+import Add from "./pages/Rent/Add";
+import Search from "./pages/Rent/Search";
 
 function App() {
   return (
@@ -21,19 +23,24 @@ function App() {
           <Link to="/citylist">citylist</Link> */}
         </nav>
         <div className="route">
-
           {/* 重定向到首页 */}
-          <Route path="/" exact render={() => {
-            return <Redirect to="/home"></Redirect>
-          }} />
-          
+          <Route
+            path="/"
+            exact
+            render={() => {
+              return <Redirect to="/home"></Redirect>;
+            }}
+          />
+
           {/* home中配置二级路由组件 */}
           <Route path="/home" component={Home} />
           <Route path="/map" component={Map} />
           <Route path="/citylist" component={citylist} />
           <Route path="/detail/:id" component={HouseDetail} />
           <Route path="/login" component={Login} />
-          <Route path="/rent" component={Rent} />
+          <Route path="/rent" exact component={Rent} />
+          <Route path="/rent/add" component={Add} />
+          <Route path="/rent/search" component={Search} />
         </div>
       </Router>
     </div>
--- src/pages/Rent/Search/index.js
+++ src/pages/Rent/Search/index.js
@@ -1,35 +1,59 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { SearchBar } from 'antd-mobile'
+import { SearchBar } from "antd-mobile";
 
-import { getCurrentCity } from '../../../utils'
+import { getCurrentCity } from "../../../utils/getCurrentCity";
 
-import styles from './index.module.css'
+import styles from "./index.module.css";
+
+import http from "../../../utils/http";
 
 export default class Search extends Component {
-  // 当前城市id
-  cityId = getCurrentCity().value
-
   state = {
     // 搜索框的值
-    searchTxt: '',
-    tipsList: []
-  }
+    searchTxt: "",
+    tipsList: [],
+    cityId: getCurrentCity(city => {
+      return city.value;
+    })
+  };
 
   // 渲染搜索结果列表
   renderTips = () => {
-    const { tipsList } = this.state
+    const { tipsList } = this.state;
 
     return tipsList.map(item => (
       <li key={item.community} className={styles.tip}>
         {item.communityName}
       </li>
-    ))
-  }
+    ));
+  };
+
+  handleValue = async v => {
+    if (v.trim().length === 0) {
+      return this.setState({
+        tipsList: [],
+        searchTxt: ""
+      });
+    }
+    this.setState({
+      searchTxt: v
+    });
+
+    const res = await http.get("/area/community", {
+      params: {
+        name: v,
+        id: this.state.cityId
+      }
+    });
+    this.setState({
+      tipsList: res.data.body
+    });
+  };
 
   render() {
-    const { history } = this.props
-    const { searchTxt } = this.state
+    const { history } = this.props;
+    const { searchTxt } = this.state;
 
     return (
       <div className={styles.root}>
@@ -38,12 +62,13 @@ export default class Search extends Component {
           placeholder="请输入小区或地址"
           value={searchTxt}
           showCancelButton={true}
-          onCancel={() => history.replace('/rent/add')}
+          onCancel={() => history.replace("/rent/add")}
+          onChange={this.handleValue}
         />
 
         {/* 搜索提示列表 */}
         <ul className={styles.tips}>{this.renderTips()}</ul>
       </div>
-    )
+    );
   }
 }
